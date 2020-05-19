cbd7cb2a60948197e03ad538a03d9678b3b2a735
Starcush <hacker0419@gmail.com>
2020-05-19 17:52:54 +0900

    FIX express cors error
     - S3와 연결을 위해 express cors 모듈을 설치.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
+
+app.use(
+  cors({
+    origin: ['http://54.180.103.96:4000/'],
+    method: ['GET', 'POST'],
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello Express');
