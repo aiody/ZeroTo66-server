f0c10cd42b2d5cf0faf08f4a984e44b07557927a
Starcush <hacker0419@gmail.com>
2020-05-20 13:17:27 +0900

    FIX cors option for S3, EC2, local
     - 미들웨어에 S3, EC2, local의 CORS 옵션을 추가

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+
+app.use(
+  cors({
+    method: ['GET', 'POST'],
+    credentials: true,
+  })
+);
+
+app.get('/', cors(corsCheck), (req, res) => {
+  res.send('Hello Express');
+});
+
+app.listen(port, () => {
+  console.log('server on 4000');
+});
+
+function corsCheck(req, callback) {
+  let corsOptions;
+  const acceptList = [
+    'http://54.180.103.96:4000',
+    'http://localhost:4000',
+    'http://zeroto66-codestates.s3-website.ap-northeast-2.amazonaws.com/',
+  ];
+  if (acceptList.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+}
