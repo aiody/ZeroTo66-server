586c13e63cfd5c6ccaea2f78e2a1ca9102724aec
Starcush <hacker0419@gmail.com>
2020-05-22 17:31:02 +0900

    ADD delete habit
     - 삭제 버튼을 통해 해당 습관이 더이상 리스트에 나오지 않도록 했습니다.
     - 습관의 중복 추가도 가능하도록 했습니다.

--- controller/habit/addhabit.js
+++ controller/habit/addhabit.js
@@ -3,20 +3,26 @@ module.exports = {
   post: (req, res) => {
     const { habitName } = req.body;
     const { id } = req.decoded;
-    habits
-      .findOrCreate({
-        where: {
+    if (habitName !== undefined) {
+      habits
+        .create({
           userId: id,
           habitName: habitName,
-        },
-      })
-      .then(async ([habit, created]) => {
-        if (!created) {
-          res.status(409).send('already exist habit');
-        } else {
-          const data = await habit.get({ plain: true });
-          res.status(201).json(data);
-        }
-      });
+        })
+        .then(async (created) => {
+          if (!created) {
+            res.status(409).send("doesn't created");
+          } else {
+            const data = await created.get({ plain: true });
+            res.status(201).json(data);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          res.sendStatus(500);
+        });
+    } else {
+      res.status(204).send('please insert habit');
+    }
   },
 };
--- controller/habit/getHabits.js
+++ controller/habit/getHabits.js
@@ -3,10 +3,14 @@ module.exports = {
   get: (req, res) => {
     const { id } = req.decoded;
     habits
-      .findAll({ where: { userId: id } })
+      .findAll({ where: { userId: id }, raw: true })
       .then((habitsData) => {
         if (habitsData && habitsData.length > 0) {
-          res.status(200).json(habitsData);
+          const filteredData = habitsData.filter((habit) => {
+            if (!habit['deletedDate']) return true;
+            return false;
+          });
+          res.status(200).json(filteredData);
         } else {
           res.status(204).send('no habit');
         }
